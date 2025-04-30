ac0358261bc10b25c3cfdbee278cc8e129bac029
Dennis Peter <peterdennis573@gmail.com>
2025-04-30 09:43:26 +0300

    Updates

--- frontend/src/components/userDashboard/Loader.tsx
+++ frontend/src/components/userDashboard/Loader.tsx
@@ -4,22 +4,19 @@ import { useEffect, useState } from "react";
 import { PacmanLoader } from "react-spinners";
 
 export default function Loader({ isLoading }: { isLoading: boolean }) {
-  const [isVisible, setIsVisible] = useState(true);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (!isLoading) {
-      
-      const timer = setTimeout(() => setIsVisible(false), 300);
-      return () => clearTimeout(timer);
-    } else {
-      setIsVisible(true);
-    }
-  }, [isLoading]);
+    setIsMounted(true);
+    return () => setIsMounted(false);
+  }, []);
 
-  if (!isVisible) return null;
+  if (!isMounted || !isLoading) {
+    return null;
+  }
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/30 bg-opacity-50 backdrop-blur-sm">
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/30 bg-opacity-50 backdrop-blur-sm">
       <PacmanLoader color="#36d7b7" size={25} />
     </div>
   );
