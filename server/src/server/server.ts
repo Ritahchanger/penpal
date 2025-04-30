ca8ecf2707ed80e7a65681c93a19c04626662838
Dennis Peter <peterdennis573@gmail.com>
2025-04-30 12:02:03 +0300

    Updates on the server.ts

--- server/src/server/server.ts
+++ server/src/server/server.ts
@@ -1,1 +1,38 @@
-console.log("Server instantiated");
+import express, {Application,Response,Request} from "express";
+
+
+import dotnev from "dotenv";
+
+
+
+const PORT : number = parseInt(process.env.PORT as string, 10) || 5000;
+
+
+
+const app : Application = express();
+
+
+
+
+
+
+
+app.use(express.json());
+
+
+
+
+
+
+dotnev.config();
+
+
+
+
+
+
+app.listen(PORT,()=>{
+
+    console.log(`${process.env.name?.toUpperCase()} is running on PORT: ${PORT}`);
+
+})
