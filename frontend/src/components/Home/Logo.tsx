99df62d8593680201bb4e09248a8b01b635b9001
Dennis Peter <peterdennis573@gmail.com>
2025-05-15 16:23:46 +0300

    Updates on the navbar

--- frontend/src/components/Home/Logo.tsx
+++ frontend/src/components/Home/Logo.tsx
@@ -4,13 +4,10 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link
-      href="/"
-      className="text-lg font-bold tracking-wider hover:opacity-80 flex items-center space-x-2"
-    >
+    <div className="text-lg font-bold tracking-wider hover:opacity-80 flex items-center space-x-2">
       <Image src="/icons/pen-tool.png" alt="Logo" width={24} height={24} />
       <span className="hidden sm:inline text-white">Bemi Editors</span>
-    </Link>
+    </div>
   );
 };
 
